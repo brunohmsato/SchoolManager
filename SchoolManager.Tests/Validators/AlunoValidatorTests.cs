556c3089c003e597c3784115e7ee0b3aea8e112f
Bruno Sato <85025347+brunohmsato@users.noreply.github.com>
2025-11-03 11:54:59 -0300

    - implementação dos testes para validators; (#1)
    * - implementação dos testes para validators;
    
    * - corrigido a exibição da mensagem no validador;
    
    ---------
    
    Co-authored-by: Bruno H. M. Sato <bruno.sato@fenoxtec.com.br>

---
+++ SchoolManager.Tests/Validators/AlunoValidatorTests.cs
@@ -0,0 +1,89 @@
+﻿using FluentAssertions;
+using SchoolManager.Application.Validators;
+using SchoolManager.Domain.Entities;
+
+namespace SchoolManager.Tests.Validators;
+
+public class AlunoValidatorTests
+{
+    private readonly AlunoValidator _validator = new();
+
+    [Fact]
+    public void Deve_Passar_Quando_DadosValidos()
+    {
+        var aluno = new Aluno
+        {
+            NomeCompleto = "João da Silva",
+            Matricula = "A001",
+            DataNascimento = new DateOnly(2000, 5, 10)
+        };
+
+        var result = _validator.Validate(aluno);
+
+        result.IsValid.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Deve_Falhar_Quando_NomeVazio()
+    {
+        var aluno = new Aluno
+        {
+            NomeCompleto = "",
+            Matricula = "A001",
+            DataNascimento = new DateOnly(2000, 5, 10)
+        };
+
+        var result = _validator.Validate(aluno);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().Contain(e => e.ErrorMessage == "O nome é obrigatório.");
+    }
+
+    [Fact]
+    public void Deve_Falhar_Quando_NomeCurto()
+    {
+        var aluno = new Aluno
+        {
+            NomeCompleto = "Jo",
+            Matricula = "A001",
+            DataNascimento = new DateOnly(2000, 5, 10)
+        };
+
+        var result = _validator.Validate(aluno);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().Contain(e => e.ErrorMessage == "O nome deve ter pelo menos 3 caracteres.");
+    }
+
+    [Fact]
+    public void Deve_Falhar_Quando_MatriculaVazia()
+    {
+        var aluno = new Aluno
+        {
+            NomeCompleto = "João da Silva",
+            Matricula = "",
+            DataNascimento = new DateOnly(2000, 5, 10)
+        };
+
+        var result = _validator.Validate(aluno);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().Contain(e => e.ErrorMessage == "A matrícula é obrigatória.");
+    }
+
+    [Fact]
+    public void Deve_Falhar_Quando_DataNascimentoFutura()
+    {
+        var aluno = new Aluno
+        {
+            NomeCompleto = "João da Silva",
+            Matricula = "A001",
+            DataNascimento = DateOnly.FromDateTime(DateTime.Today.AddDays(1))
+        };
+
+        var result = _validator.Validate(aluno);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().Contain(e => e.ErrorMessage == "Data de nascimento inválida.");
+    }
+}
---
+++ SchoolManager.Tests/Validators/DisciplinaValidatorTests.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using SchoolManager.Application.Validators;
+using SchoolManager.Domain.Entities;
+
+namespace SchoolManager.Tests.Validators;
+
+public class DisciplinaValidatorTests
+{
+    private readonly DisciplinaValidator _validator = new();
+
+    [Fact]
+    public void Deve_Passar_Quando_NomeValido()
+    {
+        var disciplina = new Disciplina { Nome = "Matemática" };
+
+        var result = _validator.Validate(disciplina);
+
+        result.IsValid.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Deve_Falhar_Quando_NomeVazio()
+    {
+        var disciplina = new Disciplina { Nome = "" };
+
+        var result = _validator.Validate(disciplina);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().Contain(e => e.ErrorMessage == "O nome da disciplina é obrigatório.");
+    }
+
+    [Fact]
+    public void Deve_Falhar_Quando_NomeMuitoCurto()
+    {
+        var disciplina = new Disciplina { Nome = "A" };
+
+        var result = _validator.Validate(disciplina);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().Contain(e => e.ErrorMessage.Contains("2 caracteres"));
+    }
+}
